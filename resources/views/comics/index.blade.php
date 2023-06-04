36cfc7086365221883386fcf407dd81c9968bddf
davidecaputo <daca9621@gmail.com>
2023-06-04 19:34:32 +0200

    Print comics

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.app')
+
+@section('content')
+<div class="h-100 bg-danger">
+    <h1 class="text-center text-white py-4">Comics</h1>
+    <div class="content">
+        <div class="row mx-5">
+            @foreach ($comics as $comic)
+            <div class="col-3 mb-5">
+                <div class="card h-100 p-4">
+                    <img src="{{$comic->image}}" alt="{{$comic->title}}" class="rounded-5">
+                    <div class="card-body">
+                        <h2>{{$comic->title}}</h2>
+                        <p>{{$comic->description}}</p>
+                        <p>{{$comic->price}}</p>
+                        <p>{{$comic->series}}</p>
+                        <p>{{$comic->type}}</p>
+                        <p>{{$comic->sale_date}}</p>
+                        <p class="fw-bold">Artisti: {{$comic->artists}}</p>
+                        <p class="fw-bold">Scrittori: {{$comic->writers}}</p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+
+@section('content')
+<h1 class="text-center">Homepage</h1>
+<h2 class="text-primary text-center"><a href="{{ route('comics.index') }}">Comics</a></h2>
+@endsection
